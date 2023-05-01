5d374793a1de9c769e01819ab62b8baa2ea4164c
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2023-05-01 21:07:28 +0200

    Added Generation Token Endpoint UserService now can request the generation of token by the auth service

--- Services/JwtUtils.cs
+++ Services/JwtUtils.cs
@@ -6,6 +6,7 @@ namespace ShareBucket.JwtMiddlewareClient.Services
     public interface IJwtUtils
     {
         public int? ValidateToken(string token);
+        public string GenerateToken(int userId);
     }
 
     public class JwtUtils : IJwtUtils
@@ -22,18 +23,33 @@ namespace ShareBucket.JwtMiddlewareClient.Services
             var client = new TokenService.TokenServiceClient(channel);
 
 
-            var reply = client.TokenValidationRequest(new TokenParam { Token = token });
-
-            if (reply.IsValid)
+            if (token != null)
             {
-                return reply.UserId;
+                var reply = client.TokenValidationRequest(new TokenParam { Token = token });
+
+                if (reply.IsValid)
+                {
+                    return reply.UserId;
+                }
+                else
+                {
+                    return null;
+                }
             }
             else
             {
                 return null;
             }
+        }
+        public string GenerateToken(int userId)
+        {
+            // Make the request to the auth service through grpc
+            var channel = GrpcChannel.ForAddress("https://localhost:7001");
+            var client = new TokenService.TokenServiceClient(channel);
 
+            var reply = client.TokenGenerationRequest(new IdUserParam { Id = userId });
 
+            return reply.Token;
         }
     }
 }
